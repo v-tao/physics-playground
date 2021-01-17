f7569d299d670788cfa4004b7fcb7a851646120b
Vivian Tao <vivian0tao@gmail.com>
2021-01-17 15:27:40 -0500

    added final velocity output and restricted y to >= 0

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import math
 #input angle, velocity, and time output position
-def kinematics_2d(theta, v, t, g=-9.813):
+def kinematics_2d(theta, v_initial, t, g=-9.813):
     theta = theta * (math.pi/180)
-    x = v*math.cos(theta)*t
-    y = v*math.sin(theta)*t+0.5*g*(t**2)
-    print(x,y)
-    return (x,y)
+    vx_initial = v_initial*math.cos(theta)
+    vy_initial = v_initial*math.sin(theta)
+    x = vx_initial*t
+    y = vy_initial*t+0.5*g*(t**2)
+    vx_final = vx_initial
+    vy_final = vy_initial + g*t
+    if y < 0:
+        y = 0
+    print("Displacement: " + str((x, y)))
+    print("Final Velocity: " + str((vx_final, vy_final)))
+    return (x,y, vx_final, vy_final)
 
 kinematics_2d(60, 30, 3)
